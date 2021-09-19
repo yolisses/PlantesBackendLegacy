59bce66a5c585c99c56df85d13b42f810337ba70
Ulisses <m.stunik@gmail.com>
2021-09-19 08:04:50 -0300

    clean schema

--- src/data/schema.js
+++ src/data/schema.js
@@ -15,48 +15,6 @@ export const typeDefs = gql`
         tags:[String]
     }
 
-    type Friend{
-        id:ID
-        firstName:String
-        lastName:String
-        gender:Gender
-        language:String
-        age:Int
-        email: String
-        contacts:[Contact]
-    }
-
-    type Contact{
-        firstName:String
-        lastName:String
-    }
-
-    type Series {
-        id:ID
-        seriesName:String
-        year:Int
-        rating:Rating
-    }
-
-    enum Rating{
-        ONE
-        TWO
-        THREE
-    }
-
-    enum Gender{
-        MALE
-        FEMALE
-        OTHER
-    }
-
-    input SeriesInput{
-        id:ID
-        seriesName:String
-        year:Int
-        rating:Rating
-    }
-
     input PlantInput{
         name:String
         description:String
@@ -68,41 +26,13 @@ export const typeDefs = gql`
         imagesCount:Int!
     }
 
-    input FriendInput{
-        id:ID
-        firstName:String
-        lastName:String
-        gender:Gender
-        language:String
-        age:Int
-        email: String
-        contacts:[ContactInput]
-    }
-
-    input ContactInput{
-        firstName:String
-        lastName:String
-    }
-
     type Query{
-        getAllFriends:[Friend]
         getPlant(id:ID):Plant
         getAllPlants:[Plant]
-        findASeries(id:ID):Series
     }
 
     type Mutation{
-        createFriend(input:FriendInput):Friend
         createPlant(input:PlantInput):Plant
-        addASeries(series:SeriesInput):Series
     }
 
 `;
-
-// Allgum dia...
-// enum Part{
-//     PLANT
-//     SEED
-//     CUTTING
-//     BULB
-// }

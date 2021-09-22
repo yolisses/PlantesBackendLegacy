c2fd8bce5681511c443ba180a5c8da159d076462
Ulisses <m.stunik@gmail.com>
2021-09-22 13:37:46 -0300

    add user creation and authentication

--- src/auth/authResolvers.js
+++ src/auth/authResolvers.js
@@ -1,25 +1,28 @@
-
 import { OAuth2Client } from 'google-auth-library';
 import { getOrCreateUser } from '../user/getOrCreateUser.js';
+import { checkNotNull } from '../utils/checkNotNull.js';
 import { generateToken } from './generateToken.js';
+
 const client = new OAuth2Client();
 
-export const authResolvers = {
-    Query: {
-        authenticateWithGoogle: async (root, { token:googleToken }) => {
-            try {
-                const ticket = await client.verifyIdToken({
-                    idToken: googleToken,
-                    audience: process.env.GOOGLE_CLIENT_ID,
-                });
-                const payload = ticket.getPayload();
-                const {name, email} = payload
+export const AuthController = {
+  async authenticateWithGoogle(req, res) {
+    const { idToken } = req.body;
+    checkNotNull(res, { idToken });
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+      const payload = ticket.getPayload();
+      const { name, email } = payload;
 
-                const user = await getOrCreateUser({name, email})
-                return generateToken({id:user.id})
-            } catch (err) {
-                console.error(err)
-            }
-        }
+      const user = await getOrCreateUser({ name, email });
+
+      const token = generateToken({ id: user.id });
+      return res.send({ token });
+    } catch (err) {
+      return res.status(400).send({ error: `${err}` });
     }
-}
+  },
+};

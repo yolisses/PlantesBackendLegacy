1526fd7de64ac531ddfeb0e727d04ee95a643f50
Ulisses <m.stunik@gmail.com>
2021-09-22 13:47:39 -0300

    fix authMiddleware

--- src/auth/authMiddleware.js
+++ src/auth/authMiddleware.js
@@ -1,24 +1,29 @@
 import jwt from 'jsonwebtoken';
 
 export function authMiddleware(req, res, next) {
-  const secret = process.env.SECRET;
+  const secret = process.env.AUTH_SECRET;
 
   const authHeader = req.headers.auth;
-
-  if (!authHeader) return res.status(401).send({ error: 'No token provided' });
+  if (!authHeader) {
+    return res.status(401).send({ error: 'No token provided' });
+  }
 
   const parts = authHeader.split(' ');
-
-  if (parts.length !== 2) return res.status(401).send({ error: 'Token error' });
+  if (parts.length !== 2) {
+    return res.status(401).send({ error: 'Token with invalid number of segments' });
+  }
 
   const [scheme, token] = parts;
-
-  if (!/^Bearer$/i.test(scheme)) { return res.status(401).send({ error: 'Malformatted token' }); }
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ error: 'Malformatted token' });
+  }
 
   jwt.verify(token, secret, (err, decoded) => {
-    if (err) return res.status(401).send({ error: 'Invalid token' });
+    if (err) {
+      return res.status(401).send({ error: err });
+    }
 
     req.user_id = decoded.id;
     return next();
   });
-};
+}
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
-import express from 'express'
-import { AuthController } from './auth/authResolvers.js'
-import { PlantController } from './plant/PlantController.js'
+import express from 'express';
+import { authMiddleware } from './auth/authMiddleware.js';
+import { AuthController } from './auth/authResolvers.js';
+import { PlantController } from './plant/PlantController.js';
 
-export const routes = express.Router()
+export const routes = express.Router();
 
-routes.get('/', (req, res) => res.send({ hello: 'world' }))
+routes.get('/plants', authMiddleware, PlantController.getAllPlants);
+routes.get('/plant/:id', PlantController.getPlant);
+routes.post('/plant', PlantController.createPlant);
 
-routes.get('/plants', PlantController.getAllPlants)
-routes.get('/plant/:id', PlantController.getPlant)
-routes.post('/plant', PlantController.createPlant)
-
-routes.post('/googlesignin', AuthController.authenticateWithGoogle)
+routes.post('/googlesignin', AuthController.authenticateWithGoogle);

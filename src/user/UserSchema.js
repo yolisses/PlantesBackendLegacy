e4074fd2bcd6455e72ed19a51761aa039711c48e
Ulisses <m.stunik@gmail.com>
2021-09-23 10:20:13 -0300

    lint

--- src/user/UserSchema.js
+++ src/user/UserSchema.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    email: {
-        type: String,
-        unique: true // `email` must be unique
-    },
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true, // `email` must be unique
+  },
 });

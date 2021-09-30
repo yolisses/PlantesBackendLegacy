2bd0f8aeef64b51624c827e545679fe98fd619a6
Ulisses <m.stunik@gmail.com>
2021-09-30 10:44:31 -0300

    create update profile (incomplete)

--- src/user/UserController.js
+++ src/user/UserController.js
@@ -20,4 +20,30 @@ export const UserController = {
     return res.send(user);
   },
 
+  async updateProfile(req, res) {
+    const { userId } = req;
+    const { name, description } = req.body;
+
+    const user = User.findById(userId);
+
+    if (!name && !description) {
+      return res.status(400).send({ error: 'Missing update values' });
+    }
+
+    const updateObj = {};
+    if (name) {
+      if (name.length < 3) {
+        return res.status(401).send({ error: 'Name with less than 3 characters' });
+      }
+      updateObj.name = name;
+    }
+
+    if (description || description === null) {
+      updateObj.description = description;
+    }
+
+    const newUser = await User.findByIdAndUpdate(userId, updateObj, { new: true }).exec();
+
+    return res.send(newUser);
+  },
 };

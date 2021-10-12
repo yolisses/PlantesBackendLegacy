64bd82e1b0389b348e340dc3d909734df8dad4dd
Ulisses <m.stunik@gmail.com>
2021-10-12 13:28:59 -0300

    toc

--- src/db/entities.js
+++ src/db/entities.js
@@ -1,17 +1,17 @@
 import './dbConnector.js';
 import mongoose from 'mongoose';
 
-import { PlantSchema } from '../plant/PlantSchema.js';
-import { UserSchema } from '../user/UserSchema.js';
-import { MessageSchema } from '../chat/MessageSchema.js';
 import { ChatSchema } from '../chat/ChatSchema.js';
+import { UserSchema } from '../user/UserSchema.js';
+import { PlantSchema } from '../plant/PlantSchema.js';
+import { MessageSchema } from '../chat/MessageSchema.js';
 
+const User = mongoose.model('Users', UserSchema);
+const Chat = mongoose.model('Chats', ChatSchema);
+const Plant = mongoose.model('Plants', PlantSchema);
+const Message = mongoose.model('Messages', MessageSchema);
 const SendingPlant = mongoose.model('SendingPlants', PlantSchema);
 const RemovedPlant = mongoose.model('RemovedPlants', PlantSchema);
-const Plant = mongoose.model('Plants', PlantSchema);
-const User = mongoose.model('Users', UserSchema);
-const Message = mongoose.model('Messages', MessageSchema);
-const Chat = mongoose.model('Chats', ChatSchema);
 
 export {
   Plant, User, SendingPlant, Message, Chat, RemovedPlant,

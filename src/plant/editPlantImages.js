2162cbd901c9f2633125e960e5c161a7bf5044da
Ulisses <m.stunik@gmail.com>
2021-10-12 17:20:56 -0300

    try plant images edit

--- src/plant/editPlantImages.js
+++ src/plant/editPlantImages.js
@@ -1,14 +1,25 @@
+import { Plant } from '../db/entities.js';
+
 export async function editPlantImages(req, res) {
   const { images } = req.body;
+  const { id } = req.params;
   const { userId } = req;
 
-  const plant = Plant;
+  const plant = await Plant.findById(id).exec();
 
-  for (const image of images) {
-
+  if (!plant) {
+    return res.status(404).send({ error: 'Plant not found' });
   }
 
-  console.error(images);
+  if (plant.userId !== userId) {
+    return res.status(403).send({ error: 'User unauthorized to change plant' });
+  }
+
+  const result = []
+  for(let image of images){
+    if(image)
+  }
+
 
   return res.send(images);
 }

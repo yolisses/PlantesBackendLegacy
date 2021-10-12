6523dfacd8cb446680dd482899910df330c65cd6
Ulisses <m.stunik@gmail.com>
2021-10-12 13:28:43 -0300

    toc

--- src/plant/editPlantImages.js
+++ src/plant/editPlantImages.js
@@ -1,5 +1,12 @@
 export async function editPlantImages(req, res) {
   const { images } = req.body;
+  const { userId } = req;
+
+  const plant = Plant;
+
+  for (const image of images) {
+
+  }
 
   console.error(images);
 
